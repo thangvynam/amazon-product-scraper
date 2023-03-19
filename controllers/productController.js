27fa5dc9a5e3c8fe886c03f1a37ce4590585146a
Nam Thang <thangvynam1808@gmail.com>
2023-03-19 16:20:31 +0700

    feat: add info call external api

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,33 @@
+const http = require("https");
+
 exports.getByKeyWord = (req, res, next) => {
-    var queryParameter = req.query;
-    console.log(queryParameter);
-    res.json(queryParameter);
+    try {
+        var queryParameter = req.query;
+        console.log(queryParameter);
+        
+        const options = {
+            "method": "GET",
+            "hostname": "ecom.webscrapingapi.com",
+            "port": null,
+            "path": "/v1?engine=amazon&api_key=<YOUR_API_KEY>&type=search&q=memory%20ca",
+            "headers": {}
+        };
+        
+        const request = http.request(options, function (res) {
+        const chunks = [];
+        
+        res.on("data", function (chunk) {
+            chunks.push(chunk);
+        });
+        
+        res.on("end", function () {
+            const body = Buffer.concat(chunks);
+            console.log(body.toString());
+        });
+        });
+        
+        request.end();
+    } catch (error) {
+        next(error);
+    }
 };
