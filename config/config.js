e8d2e3d2fa7442fb2b38a7e1c9d694d66d86fe51
Nam Thang <thangvynam1808@gmail.com>
2023-03-25 15:57:59 +0700

    feat: validate username

--- config/config.js
+++ config/config.js
@@ -7,6 +7,7 @@ const config = {
   app: {
     tokenSecret: process.env.TOKEN_SECRET,
     env: process.env.NODE_ENV, // 'dev' or 'prod'
+    userName: process.env.USER_NAME.split(','),
   },
   vendor: {
     hostVendor: process.env.HOST_VENDOR,
--- controllers/authController.js
+++ controllers/authController.js
@@ -5,8 +5,20 @@ function generateAccessToken(id) {
   return jsonwebtoken.sign({ id }, config.app.tokenSecret, { expiresIn: '7d' });
 }
 
+function validate(userName) {
+  return config.app.userName.includes(userName);
+}
+
 export default function login(req, res, next) {
   try {
+    const userName = req.body.username;
+    if (!validate(userName)) {
+      return res.status(401).json({
+        ok: false,
+        error: 'Unauthorized',
+      });
+    }
+
     const token = generateAccessToken(req.body.username);
     res.json({
       username: req.body.username,
