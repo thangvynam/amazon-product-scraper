37e1c3dfe338452c9a0fad9e776f9f6f1fb08db4
Nam Thang <thangvynam1808@gmail.com>
2023-04-10 21:08:49 +0700

    feat: update pre-commit

--- hooks/preCommitChecks.sh
+++ hooks/preCommitChecks.sh
@@ -3,7 +3,7 @@
 # This script is run before every commit. It checks for the following:
 # - If the commit message is empty
 
-#if ! npm run lint ; then
-#  echo "Linting failed. Please fix the errors and try again."
-#  exit 1
-#fi
+if ! npm run lint ; then
+  echo "Linting failed. Please fix the errors and try again."
+  exit 1
+fi
